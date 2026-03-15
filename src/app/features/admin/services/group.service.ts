ea8ed40843330d9f7573de7f88d8dcf9807978a9
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-03-15 22:48:37 +0000

    l'implémentation des services group , post,user ainsi que le service inscription

--- src/app/features/admin/services/group.service.ts
+++ src/app/features/admin/services/group.service.ts
@@ -1,53 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { GroupResponse, GroupRequest } from '../models/group.model';
 import { environment } from '../../../../environments/environment';
 
-export interface GroupRequest {
-  name: string;
-  description: string;
-  coachId: number;
-  memberIds?: number[];
-}
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupService {
 
-export interface GroupResponse {
-  id: number;
-  name: string;
-  description: string;
-  coachId: number;
-  coachName: string;
-  memberCount: number;
-  createdAt: string;
-}
-
-@Injectable({ providedIn: 'root' })
-export class AdminGroupService {
-
-  private readonly base = `${environment.apiUrl}/api/admin/groups`;
+  private apiUrl = `${environment.apiUrl}/api/admin/groups`;
 
   constructor(private http: HttpClient) { }
+
   getGroups(): Observable<GroupResponse[]> {
-    return this.http.get<GroupResponse[]>(this.base);
+    return this.http.get<GroupResponse[]>(this.apiUrl);
   }
 
   getGroupById(id: number): Observable<GroupResponse> {
-    return this.http.get<GroupResponse>(`${this.base}/${id}`);
+    return this.http.get<GroupResponse>(`${this.apiUrl}/${id}`);
   }
 
   getGroupsByCoach(coachId: number): Observable<GroupResponse[]> {
-    return this.http.get<GroupResponse[]>(`${this.base}/coach/${coachId}`);
+    return this.http.get<GroupResponse[]>(`${this.apiUrl}/coach/${coachId}`);
   }
 
-  createGroup(data: GroupRequest): Observable<GroupResponse> {
-    return this.http.post<GroupResponse>(this.base, data);
+  createGroup(group: GroupRequest): Observable<GroupResponse> {
+    return this.http.post<GroupResponse>(this.apiUrl, group);
   }
 
-  updateGroup(id: number, data: GroupRequest): Observable<GroupResponse> {
-    return this.http.put<GroupResponse>(`${this.base}/${id}`, data);
+  updateGroup(id: number, group: GroupRequest): Observable<GroupResponse> {
+    return this.http.put<GroupResponse>(`${this.apiUrl}/${id}`, group);
   }
 
   deleteGroup(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.base}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
-
 }
